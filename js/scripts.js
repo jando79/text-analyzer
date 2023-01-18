58e7849c85bc7a74b121565e5cc0803448995a93
David Jandron <jandron1211@gmail.com>
2023-01-18 14:33:14 -0800

    ADD additional functionality into word counter function in busienss logic

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,14 @@ function wordCounter(text) {
   if (text.trim().length === 0) {
     return 0;
   }
+  let wordCount = 0;
+  const textArray = text.split(" ");
+  textArray.forEach(function(element) {
+    if (!Number(element)) {
+      wordCount++;
+    }
+  });
+  return wordCount;
 }
 
 function numberOfOccurrencesInText(word, text) {
@@ -23,11 +31,11 @@ window.onload = function() {
   const omitOffensiveWords = ["zoinks", "muppeteer", "biffaroni", "loopdaloop"]
   const textArray = text.split(" ");
   console.log(textArray)
-  for (i=0; i <= textArray.length-1; i++) {
+  for (i=0; i <= textArray.length-1; i++) {  //makes index equaL LENGTH. variable "i" is iterator and pointer (i.e., where to start) "i" = 0 because that is where index starts
     if(omitOffensiveWords.includes(textArray[i].toLowerCase())) {
       console.log(textArray[i]);
       textArray[i] = "****"   
-      console.log(textArray) 
+      console.log(textArray) //log to track individual changes
     }
   }
 }
