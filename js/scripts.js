69947a94083ba6056c593b7025246d6ac14bf33c
David Jandron <jandron1211@gmail.com>
2023-01-17 16:05:34 -0800

    complete test in readme and added additional code in js to solve tests
    Co-authored-by: Kaleb Taylor <kaleb.taylor1976@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -4,16 +4,15 @@ function wordCounter(text) {
   if (text.trim().length === 0) {
     return 0;
   }
-  let wordCount = 0;
+}
+
+function numberOfOccurrencesInText(word, text) {
   const textArray = text.split(" ");
-  textArray.forEach(function(word) {
-    if (!Number(element)) {
-    wordCount++;
+  let wordCount = 0;
+  textArray.forEach(function(element) {
+    if (element.toLowerCase().includes(word.toLowerCase())) {
+      wordCount++;
     }
   });
   return wordCount;
-}
-
-function numberOfOccurencesInText(word, text) {
-  return 0;
 }
